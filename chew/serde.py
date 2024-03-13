080b0a6733ce2283e509aeb1f994229c71a012f5
speedcell4 <speedcell4@gmail.com>
2024-03-14 03:04:12 +0900

    Feat: Add retry

--- chew/serde.py
+++ chew/serde.py
@@ -1,20 +1,21 @@
 import json
+from json import JSONDecodeError
 from pathlib import Path
 from typing import Any
 
-
-def load_json(path: Path, default: Any = None) -> Any:
-    try:
-        with path.open(mode='r', encoding='utf-8') as fp:
-            return json.load(fp=fp)
-    except FileNotFoundError as error:
-        if default is not None:
-            return default
-        raise error
-
-
 ARGS_FILENAME = 'args.json'
 SOTA_FILENAME = 'sota.json'
+
+
+def load_json(path: Path) -> Any:
+    with path.open(mode='r', encoding='utf-8') as fp:
+        for _ in range(5):
+            try:
+                return json.load(fp=fp)
+            except JSONDecodeError:
+                pass
+
+    raise JSONDecodeError
 
 
 def load_args(out_dir: Path, name: str = ARGS_FILENAME) -> Any:
